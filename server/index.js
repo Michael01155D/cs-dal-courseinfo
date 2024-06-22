028193a77409c08fb9b1010478ea4d6a5960a0a4
Michael Mroz <michaelmroz01111@gmail.com>
2024-06-22 11:55:10 -0300

    set up and tested connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config();
+const mongoose = require('mongoose');
 
+dotenv.config();
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT;
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Hello")
+})
+
+server.listen(PORT, () => {
+    console.log("Server connected on port:", PORT);
+})
+
+mongoose.connect(MONGO_URL).then(result => {
+    console.log("Connected to MongoDB!");
+}).catch(e => {console.log("Connection to MongoDB Failed, error:", e.message)})
