2e7cfde6ffbf69b3dba80622a379d44ffbfb70f4
Michael Mroz <michaelmroz01111@gmail.com>
2024-07-25 17:33:12 -0300

    added state to CourseByYear to render different background color for <aside>

--- client/src/components/CoursesByYear.jsx
+++ client/src/components/CoursesByYear.jsx
@@ -5,19 +5,24 @@ import { useEffect, useState } from "react";
 const CoursesByYear = () => {
     const { year } = useParams();
     const [yearInfo, setYearInfo] = useState();
+    const [yearBgColor, setYearBgColor] = useState("white");
     useEffect(() => {
         switch(year) {
             case "first": 
                 setYearInfo("For first year students, courses are designed to introduce foundational CS concepts.");
+                setYearBgColor("rgba(144, 238, 144, 0.352)");
                 break;
             case "second":
                 setYearInfo("In the second year, course choices are very limited as the year has many degree requirements. One common piece of advice is to not pair 2122 and 2115 together!");
+                setYearBgColor("rgba(140, 217, 243, 0.352)");
                 break;
             case "third":
                 setYearInfo("Considered by many CS students to be the hardest year, third year contains several extremely challenging courses that build on the foundations acquired. Buckle up, you've got this!");
+                setYearBgColor("rgba(229, 43, 43, 0.352)");
                 break;
             case "fourth": 
                 setYearInfo("The final stretch! Fourth year offers students a lot of freedom to complete their degree based on their areas of interest.")
+                setYearBgColor("rgba(255, 234, 1, 0.352");
                 break;
             default:
                 break;
@@ -30,8 +35,7 @@ const CoursesByYear = () => {
                 <h2>{year[0].toUpperCase() + year.substring(1)}-Year Courses</h2>
             </header>
             <main id='byYearContainer'>
-                WIP here
-                <aside style={`--year: ${year}`} id='aboutYear'>
+                <aside style={{backgroundColor: yearBgColor}} id='aboutYear'>
                     {yearInfo}
                 </aside>
                 <section id='yearCoursesList'>
