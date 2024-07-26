b887312657ae70b4db8460b10e0396d4eca1401c
Michael Mroz <michaelmroz01111@gmail.com>
2024-07-26 18:21:28 -0300

    complete basic browser layout. next todo: make mobile layout

--- client/src/components/SignUpPage.jsx
+++ client/src/components/SignUpPage.jsx
@@ -1,16 +1,21 @@
 import UserForm from "./UserForm"
 import '../styles/SignUpPage.css'
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import NotificationMessage from "./NotificationMessage"
 const SignUpPage = () => {
-
+    const [isError, setIsError] = useState(false);
+    const [displayMsg, setDisplayMsg] = useState("");
     const handleRegister = () => {
         console.log('this will do stuff');
     }
 
     return(
         <main id='signUpPageContainer'>
-            <NotificationMessage message={"test"} isError={true}/>
+            { displayMsg.length > 0 ?  
+                <NotificationMessage message={displayMsg} isError={isError}/>
+                :<></>
+             }
             <UserForm requestHandler={handleRegister} formAction={"Create Account"}/>
             <Link id='loginLink' to="/login"> Return to Login Page</Link>
         </main>
